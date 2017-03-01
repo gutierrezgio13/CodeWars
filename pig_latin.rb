025580e544e72156099ccf84f2cf0916af9495b3
Giovanni Gutierrez <gutierrezgio13@gmail.com>
2017-03-01 11:16:27 -0800

    Fixed bug. Now consideres punctuation.

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,21 +1,26 @@
 def pig_it(text)
+  punc = ".,/?':!"
   words = text.split(' ')
   arr = []
   words.each do |word|
-    #p word
-    word = word.chars
-    #puts "Chars in word: #{word}"
-    first_letter = word.shift
-    #puts "first letter is #{first_letter}"
-    latin = first_letter + "ay"
-    #puts "latin version is #{latin}"
-    word = word.join + latin
-    arr << word
-    #puts "word with latin: #{word}"
-    #p word
+    if punc.include?(word)
+      arr << word
+    else
+      word = word.chars
+      #puts "Chars in word: #{word}"
+      first_letter = word.shift
+      #puts "first letter is #{first_letter}"
+      latin = first_letter + "ay"
+      #puts "latin version is #{latin}"
+      word = word.join + latin
+      arr << word
+      #puts "word with latin: #{word}"
+      #p word
   end
-  p arr
-  arr = arr.join(' ')
+end
+p arr
+arr = arr.join(' ')
+p arr
 end
 
-pig_it("Pig latin is cool") #=> igPay atinlay siay oolcay
+pig_it('Pig latin is cool')
